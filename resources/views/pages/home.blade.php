c5be0bb51d2fe624de862546e9e7b431290f73c6
vanditha <vn245@njit.edu>
2018-10-16 19:59:14 -0400

    Task : Creating the home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.default')
 @section('content')
-<br>
-<br>
-<br>
-<h1>Welcome to the home page</h1>
+    <br>
+    <br>
+    <br>
+    <h1>NJIT Innovation center</h1>
+    <p>The New Jersey Institute of technology Innovation Institute is an NJIT corporation focused on helping its students work on their ideas to turn them into reality helping them to build their own start-ups. The innovation center nurtures the ideas of students and helps them stand on their to work on their dream projects.</p>
+    <a type="button" class="btn btn-lg btn-default" href="http://njii.com/"><span class="glyphicon glyphicon-flash"></span> Get to know more about NJITIC</a>
+
+   
 
 
 @stop
