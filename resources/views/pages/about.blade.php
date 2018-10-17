3ce3d068b9d919fe98aca7a3affb32ab2240b631
vanditha <vn245@njit.edu>
2018-10-16 20:07:02 -0400

    Task : Adding content to the about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,10 +1,25 @@
 @extends('layouts.default')
 @section('content')
-<br>
-<br>
-<br>
+    <br>
+    <br>
+    <br>
 
-<h1> Welcome to about page</h1>
+    <div class="row text-left">
+        <div class="col-md-8">
+
+
+            <p>
+            <hr><h4>Blackdoves</h4> Blackdoves works on a manufacturing unit where its going to be contributing 40% of its income to the schools of underprivileged  and contribute to the schools development.</p>
+
+            <p><h4>Doctors Forum</h4> This project is working on getting the doctors around the country under one roof where they can discuss and work on their ideas for betterment of the soceity.</p>
+
+            <p><h4>Eventhost</h4> Students in this project work on organinising various events around the university. The team has already planned around 150 events till now and that they are doing extremely well in organising the events in the days to come as well.</p>
+            </h4>
+
+        </div>
+
+
+    </div> <!-- row -->
 
 
 
