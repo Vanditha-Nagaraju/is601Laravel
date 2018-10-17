8438fe5de4141eb9bd20576bf573ccb9077e410c
vanditha <vn245@njit.edu>
2018-10-16 20:04:12 -0400

    Feature : Adding a table to the home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,7 +7,63 @@
     <p>The New Jersey Institute of technology Innovation Institute is an NJIT corporation focused on helping its students work on their ideas to turn them into reality helping them to build their own start-ups. The innovation center nurtures the ideas of students and helps them stand on their to work on their dream projects.</p>
     <a type="button" class="btn btn-lg btn-default" href="http://njii.com/"><span class="glyphicon glyphicon-flash"></span> Get to know more about NJITIC</a>
 
-   
+    <div class="container">
+        <div class="alert alert-info text-center" role="alert">
+            <h3> Details of few students and their projects enrolled in NJITIC</h3>
+
+            <table class="table">
+                <thead class="thead-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First Name</th>
+                    <th scope="col">Last Name</th>
+                    <th scope="col">Email Id</th>
+                    <th scope="col">Startup name</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Jeff</td>
+                    <td>Marc</td>
+                    <td>Jeff@njit</td>
+                    <td>Blackdoves</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Lisa</td>
+                    <td>Thara</td>
+                    <td>lisa@njit</td>
+                    <td>Doctors forum</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Lary</td>
+                    <td>thomas</td>
+                    <td>larythomas@njit</td>
+                    <td>Eventhost</td>
+                </tr>
+                <tr>
+                    <th scope="row">4</th>
+                    <td>Jessica</td>
+                    <td>Oraldo</td>
+                    <td>Jorlido@njit</td>
+                    <td>Starton</td>
+                </tr>
+                <tr>
+                    <th scope="row">5</th>
+                    <td>carolina</td>
+                    <td>winsor</td>
+                    <td>cwinsor@njit</td>
+                    <td>Bootstrap</td>
+                </tr>
+                </tbody>
+            </table>
+
+            </ul>
+        </div>
+    </div>
+    </div>
 
 
 @stop
